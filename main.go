21886d45997b3ae1a2eeb26e3d7fb9852df78ee4
neginegi-hue <neginegi_hue@gmail.com>
2023-06-21 23:54:02 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repository"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+	"go-rest-api/validator"
+)
+
+func main() {
+	db := db.NewDB()
+	uservalidtor := validator.NewUserValidator()
+	taskvalidtor := validator.NewTaskValidator()
+	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository, uservalidtor)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskvalidtor)
+	userController := controller.NewUserController(userUsecase)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(userController, taskController)
+	e.Logger.Fatal(e.Start(":8080"))
+}
